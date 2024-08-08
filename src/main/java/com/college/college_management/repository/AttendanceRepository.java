1b699fedb49f5ed15a6b34fc2de4eee1dc6ee942
Debyani Ghosh <168951875+Debyani02@users.noreply.github.com>
2024-08-08 13:12:59 +0530

    Delete src/main directory

--- src/main/java/com/college/college_management/repository/AttendanceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.college.college_management.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.college.college_management.pojo.Attendance;
-
-public interface AttendanceRepository extends JpaRepository<Attendance, Integer>{
-
-}
--- src/main/java/com/college/college_management/repository/ResultRepository.java
+++
@@ -1,9 +0,0 @@
-package com.college.college_management.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.college.college_management.pojo.Result;
-
-public interface ResultRepository extends JpaRepository<Result,Integer>{
-
-}
